2aedb6ba94b6afad1093272dde7aa9c4bb70f078
Siddharth Narayanan <sid@futurehouse.org>
2025-09-24 17:41:52 -0500

    remote env client app

---
+++ resources_servers/aviary/client_app.py
@@ -0,0 +1,27 @@
+from pydantic import model_validator
+
+from aviary.core import TaskDatasetClient, TaskEnvironmentClient
+from resources_servers.aviary.app import AviaryResourcesServer, AviaryResourcesServerConfig
+
+
+class AviaryClientAppConfig(AviaryResourcesServerConfig):
+    server_url: str
+    request_timeout: float | None = 300.0
+    api_key: str | None = None
+
+
+class AviaryClientApp(AviaryResourcesServer[TaskEnvironmentClient, TaskDatasetClient]):
+    config: AviaryClientAppConfig
+    dataset: TaskDatasetClient
+
+    @model_validator(mode="before")
+    @classmethod
+    def load_dataset(cls, data: dict) -> dict:
+        if "dataset" not in data:
+            config = data["config"] = AviaryClientAppConfig.model_validate(data.get("config", {}))
+            data["dataset"] = TaskDatasetClient(**config.model_dump())
+        return data
+
+
+if __name__ == "__main__":
+    AviaryClientApp.run_webserver()
