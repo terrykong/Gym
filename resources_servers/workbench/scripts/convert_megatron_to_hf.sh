3365618cb11d0b51c317383d5808fa1900a96868
abhibha-nvidia <abhibhag@nvidia.com>
2025-09-25 14:57:38 -0700

    path to dc_to_hf changed

--- resources_servers/workbench/scripts/convert_megatron_to_hf.sh
+++ resources_servers/workbench/scripts/convert_megatron_to_hf.sh
@@ -28,7 +28,7 @@ srun -A llmservice_modelalignment_sft \
   -t 02:00:00 \
   --pty bash -c "
     cd $REPO_PATH
-    uv run --extra mcore python  examples/convert_dcp_to_hf.py  \
+    uv run --extra mcore python  examples/converters/convert_dcp_to_hf.py  \
       --config="${CKPT_DIR}/config.yaml" \
       --dcp-ckpt-path="${CKPT_DIR}/policy/weights" \
       --hf-ckpt-path="${CKPT_DIR}-hf" rsync -ahP ${CKPT_DIR}/policy/tokenizer ${CKPT_DIR}-hf/ "
