2899258b9d59babe5454c90577647369a7bba00a
Siddharth Narayanan <sid@futurehouse.org>
2025-09-24 19:02:11 -0500

    allow server url/api key to fall back to env vars

--- resources_servers/aviary/client_app.py
+++ resources_servers/aviary/client_app.py
@@ -1,13 +1,32 @@
-from pydantic import model_validator
+import os
+
+from pydantic import field_validator, model_validator
 
 from aviary.core import TaskDatasetClient, TaskEnvironmentClient
 from resources_servers.aviary.app import AviaryResourcesServer, AviaryResourcesServerConfig
 
 
 class AviaryClientAppConfig(AviaryResourcesServerConfig):
-    server_url: str
+    server_url: str | None = None
     request_timeout: float | None = 300.0
     api_key: str | None = None
+
+    @field_validator("server_url")
+    @classmethod
+    def validate_server_url(cls, v: str | None) -> str | None:
+        if v is None:
+            return os.getenv("AVIARY_SERVER_URL")
+        return v
+
+    @field_validator("api_key")
+    @classmethod
+    def validate_api_key(cls, v: str | None) -> str | None:
+        if v is None:
+            # Note that if AVIARY_API_KEY is not set, we will return
+            # None, which is also valid - assuming the server does
+            # not have auth enabled.
+            return os.getenv("AVIARY_SERVER_API_KEY")
+        return v
 
 
 class AviaryClientApp(AviaryResourcesServer[TaskEnvironmentClient, TaskDatasetClient]):
