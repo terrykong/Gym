d188ba359aa886fe71f2f843301b8906539d0568
Abhibha Gupta <abhibhag@nvidia.com>
2025-09-30 01:28:51 -0700

    batch_block1
    Signed-off-by: Abhibha Gupta <abhibhag@nvidia.com>

--- resources_servers/workbench/scripts/run_temp_penguin.sh
+++ resources_servers/workbench/scripts/run_temp_penguin.sh
@@ -1,6 +1,6 @@
 WANDB_API_KEY=ac17639e76dcbd0dbb3aea36aec1ae8286de56bc \
 SLURM_ACCOUNT=llmservice_modelalignment_sft \
-SLURM_PARTITION=batch \
+SLURM_PARTITION=batch_block1 \
 EXP_NAME=penguin_grpo/qwen3_4binstruct/8nodes/grpo-qwen3-4b-instruct-pre-cot \
 NUM_ACTOR_NODES=8 \
 REPO_LOCATION=/lustre/fsw/portfolios/llmservice/users/abhibhag/nemo-rl \
@@ -11,7 +11,7 @@ REPO_LOCATION=/lustre/fsw/portfolios/llmservice/users/abhibhag/nemo-rl \
 
 WANDB_API_KEY=ac17639e76dcbd0dbb3aea36aec1ae8286de56bc \
 SLURM_ACCOUNT=llmservice_modelalignment_sft \
-SLURM_PARTITION=batch \
+SLURM_PARTITION=batch_block1 \
 EXP_NAME=penguin_grpo/qwen3_4binstruct/8nodes/grpo-qwen3-4b-instruct-pre-cot \
 NUM_ACTOR_NODES=8 \
 REPO_LOCATION=/lustre/fsw/portfolios/llmservice/users/abhibhag/nemo-rl \
@@ -22,7 +22,7 @@ REPO_LOCATION=/lustre/fsw/portfolios/llmservice/users/abhibhag/nemo-rl \
 
 WANDB_API_KEY=ac17639e76dcbd0dbb3aea36aec1ae8286de56bc \
 SLURM_ACCOUNT=llmservice_modelalignment_sft \
-SLURM_PARTITION=batch \
+SLURM_PARTITION=batch_block1 \
 EXP_NAME=penguin_grpo/qwen3_4binstruct/8nodes/grpo-qwen3-4b-instruct-pre-cot \
 NUM_ACTOR_NODES=8 \
 REPO_LOCATION=/lustre/fsw/portfolios/llmservice/users/abhibhag/nemo-rl \
