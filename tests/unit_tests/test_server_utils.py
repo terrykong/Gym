0d80c409955a134e954128540537b3a360be2175
Sugam Devare <sdevare@nvidia.com>
2025-10-22 18:21:38 -0700

    fix: tests
    Signed-off-by: Sugam Devare <sdevare@nvidia.com>

--- tests/unit_tests/test_server_utils.py
+++ tests/unit_tests/test_server_utils.py
@@ -17,6 +17,7 @@ from pytest import MonkeyPatch, raises
 
 import nemo_gym.global_config
 import nemo_gym.server_utils
+from nemo_gym import PARENT_DIR
 from nemo_gym.global_config import (
     NEMO_GYM_CONFIG_PATH_ENV_VAR_NAME,
 )
@@ -193,7 +194,7 @@ class TestServerUtils:
 
         ray_is_initialized_mock.assert_called_once()
         get_global_config_dict_mock.assert_called_once()
-        ray_init_mock.assert_called_once_with(address="ray://test-address:10001", ignore_reinit_error=True)
+        ray_init_mock.assert_called_once_with(address="ray://test-address:10001", ignore_reinit_error=True, runtime_env={'working_dir': PARENT_DIR})
 
     def test_initialize_ray_without_address(self, monkeypatch: MonkeyPatch) -> None:
         ray_is_initialized_mock = self._mock_ray_return_value(monkeypatch, False)
@@ -210,4 +211,5 @@ class TestServerUtils:
 
         ray_is_initialized_mock.assert_called_once()
         get_global_config_dict_mock.assert_called_once()
-        ray_init_mock.assert_called_once_with(ignore_reinit_error=True)
+
+        ray_init_mock.assert_called_once_with(ignore_reinit_error=True,runtime_env={'working_dir': PARENT_DIR})
