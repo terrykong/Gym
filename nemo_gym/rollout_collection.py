2dff0346bce43c3c4fd9d6cf82af1434a09da94b
Peter Jin <pjin@nvidia.com>
2025-10-28 16:57:45 -0700

    Preserve the log about interleaved repeat.
    Signed-off-by: Peter Jin <pjin@nvidia.com>

--- nemo_gym/rollout_collection.py
+++ nemo_gym/rollout_collection.py
@@ -79,6 +79,7 @@ class RolloutCollectionHelper(BaseModel):  # pragma: no cover
                     )
                 ]
                 print(f"Found {len(rows)} total rows ({config.num_repeats} repeats per original row)!")
+                print("(Repeating rows in an interleaved pattern from abc to aabbcc)")
             else:
                 rows = [(row_idx, 0, row) for row_idx, row in zip(range_iterator, map(json.loads, input_dataset))]
                 print(f"Found {len(rows)} rows!")
