3b5b88775a45932ed3a562599660de1dcccea9b7
Peter Jin <pjin@nvidia.com>
2025-10-30 11:37:45 -0700

    No concurrency limit by default.
    Signed-off-by: Peter Jin <pjin@nvidia.com>

--- resources_servers/equivalence_llm_judge/app.py
+++ resources_servers/equivalence_llm_judge/app.py
@@ -59,7 +59,7 @@ class LLMJudgeResourcesServerConfig(BaseResourcesServerConfig):
     judge_model_server: ModelServerRef
     judge_responses_create_params: NeMoGymResponseCreateParamsNonStreaming
 
-    judge_endpoint_max_concurrency: Optional[int] = 128
+    judge_endpoint_max_concurrency: Optional[int] = None
 
     judge_system_message: Optional[str] = None
     judge_prompt_template: str
