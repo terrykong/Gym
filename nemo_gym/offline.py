b78eacf6893a078e716e9be6f8374ae7b64b4cdc
Peter Jin <pjin@nvidia.com>
2025-10-31 12:20:08 -0700

    Offline verification.
    Signed-off-by: Peter Jin <pjin@nvidia.com>

--- nemo_gym/offline.py
+++ nemo_gym/offline.py
@@ -13,15 +13,14 @@
 # limitations under the License.
 import asyncio
 import json
-from asyncio import Lock, Semaphore
 from collections import Counter
 from contextlib import nullcontext
 from itertools import count, product
-from typing import Any, Dict, List, Optional
+from typing import Dict, List, Optional
 
 from pydantic import BaseModel, Field
-from tqdm.asyncio import tqdm
-from tqdm import tqdm as sync_tqdm
+from tqdm.asyncio import tqdm as tqdm_asyncio
+from tqdm import tqdm
 
 from nemo_gym.config_types import BaseNeMoGymCLIConfig, BaseServerConfig
 from nemo_gym.server_utils import (
@@ -34,29 +33,25 @@ from nemo_gym.server_utils import (
 )
 
 
-class RolloutCollectionConfig(BaseNeMoGymCLIConfig):
+class VerifyOfflineConfig(BaseNeMoGymCLIConfig):
     """
-    Perform a batch of rollout collection.
+    Perform a batch of offline verification.
     """
 
-    agent_name: str = Field(description="The agent to collect rollouts from.")
+    server_name: str = Field(description="The resources server to use for verifying samples.")
     input_jsonl_fpath: str = Field(
-        description="The input data source to use to collect rollouts, in the form of a file path to a jsonl file."
+        description="The input data source of samples to verify, in the form of a file path to a jsonl file."
     )
     output_jsonl_fpath: str = Field(description="The output data jsonl file path.")
     limit: Optional[int] = Field(
-        default=None, description="Maximum number of examples to load and take from the input dataset."
+        default=None, description="Maximum number of samples to load and take from the input dataset."
     )
     num_repeats: Optional[int] = Field(
         default=None,
-        description="The number of times to repeat each example to run. Useful if you want to calculate mean@k e.g. mean@4 or mean@16.",
+        description="The number of times to repeat each sample to run. Useful if you want to calculate mean@k e.g. mean@4 or mean@16.",
     )
     num_samples_in_parallel: Optional[int] = Field(
         default=None, description="Limit the number of concurrent samples running at once."
-    )
-    responses_create_params: Dict[str, Any] = Field(
-        default_factory=dict,
-        description="Overrides for the responses_create_params e.g. temperature, max_output_tokens, etc.",
     )
     remove_reward: Optional[bool] = Field(
         default=None,
@@ -64,12 +59,12 @@ class RolloutCollectionConfig(BaseNeMoGymCLIConfig):
     )
     enable_cache: Optional[bool] = Field(
         default=None,
-        description="Enable caching for robust rollout collection.",
+        description="Enable caching for restartable offline verification.",
     )
 
 
-class RolloutCollectionHelper(BaseModel):  # pragma: no cover
-    async def run_from_config(self, config: RolloutCollectionConfig):
+class VerifyOfflineHelper(BaseModel):  # pragma: no cover
+    async def run_from_config(self, config: VerifyOfflineConfig):
         range_iterator = count()
         if config.limit:
             range_iterator = range(config.limit)
@@ -91,41 +86,36 @@ class RolloutCollectionHelper(BaseModel):  # pragma: no cover
 
         semaphore = nullcontext()
         if config.num_samples_in_parallel:
-            semaphore = Semaphore(config.num_samples_in_parallel)
+            semaphore = asyncio.Semaphore(config.num_samples_in_parallel)
 
         server_client = self.setup_server_client()
 
-        tqdm_miniters = 1
-        if False:
-            tqdm_miniters = 10
-            print(
-                f"The tqdm progress bar will only update every {tqdm_miniters} samples that finish to ensure that you are not being spammed."
-            )
-
-        if config.responses_create_params:
-            print(f"Overriding responses_create_params fields with {config.responses_create_params}")
+        tqdm_miniters = 10
+        print(
+            f"The tqdm progress bar will only update every {tqdm_miniters} samples that finish to ensure that you are not being spammed."
+        )
 
         cache_key_set = set()
 
         if config.enable_cache:
-            print("Reading cached rollouts...", flush=True)
+            print("Reading cached verifications...", flush=True)
             try:
                 with open(config.output_jsonl_fpath, "r") as f:
-                    for line in sync_tqdm(f, total=len(rows)):
+                    for line in tqdm(f, total=len(rows)):
                         item = json.loads(line)
-                        assert "_rollout_cache_key" in item
-                        item_cache_key = item["_rollout_cache_key"]
+                        assert "_verify_cache_key" in item
+                        item_cache_key = item["_verify_cache_key"]
                         row_idx = item_cache_key["row_idx"]
                         rep_idx = item_cache_key["rep_idx"]
                         cache_key_set.add((row_idx, rep_idx))
             except OSError:
                 pass
-            print(f"Found {len(cache_key_set)} cached rollouts.", flush=True)
+            print(f"Found {len(cache_key_set)} cached verifications.", flush=True)
 
-        print("Starting rollout collection...", flush=True)
+        print("Starting offline verification...", flush=True)
 
         metrics = Counter()
-        write_lock = Lock()
+        write_lock = asyncio.Lock()
         write_file = open(config.output_jsonl_fpath, "a")
 
         def _filter_row(row: tuple) -> bool:
@@ -137,9 +127,9 @@ class RolloutCollectionHelper(BaseModel):  # pragma: no cover
 
         async def _post_coroutine(row: tuple) -> None:
             row_idx, rep_idx, row = row
-            row["responses_create_params"] = row["responses_create_params"] | config.responses_create_params
+            row["responses_create_params"] = row["responses_create_params"]
             async with semaphore:
-                response = await server_client.post(server_name=config.agent_name, url_path="/run", json=row)
+                response = await server_client.post(server_name=config.server_name, url_path="/verify", json=row)
                 if config.enable_cache:
                     try:
                         await raise_for_status(response)
@@ -152,8 +142,8 @@ class RolloutCollectionHelper(BaseModel):  # pragma: no cover
                 if config.remove_reward:
                     result.pop("reward", None)
                 if config.enable_cache:
-                    assert "_rollout_cache_key" not in result
-                    result["_rollout_cache_key"] = {
+                    assert "_verify_cache_key" not in result
+                    result["_verify_cache_key"] = {
                         "row_idx": row_idx,
                         "rep_idx": rep_idx,
                     }
@@ -161,12 +151,16 @@ class RolloutCollectionHelper(BaseModel):  # pragma: no cover
                     print(json.dumps(result), file=write_file, flush=True)
                 metrics.update({k: v for k, v in result.items() if isinstance(v, (int, float))})
 
-        await tqdm.gather(*filter(_post_coroutine, filter(_filter_row, rows)), desc="Collecting rollouts", miniters=tqdm_miniters)
+        await tqdm_asyncio.gather(
+            *filter(_post_coroutine, filter(_filter_row, rows)),
+            desc="Collecting rollouts",
+            miniters=tqdm_miniters,
+        )
 
         write_file.flush()
         write_file.close()
 
-        print("Done rollout collection.", flush=True)
+        print("Done offline verification.", flush=True)
 
         avg_metrics = {k: v / len(rows) for k, v in metrics.items()}
 
@@ -175,14 +169,7 @@ class RolloutCollectionHelper(BaseModel):  # pragma: no cover
     async def run_examples(
         self, examples: List[Dict], head_server_config: Optional[BaseServerConfig] = None
     ) -> List[Dict]:
-        server_client = self.setup_server_client(head_server_config)
-
-        async def _post_subroutine(row: Dict) -> Dict:
-            res = await server_client.post(server_name=row.pop("agent_ref")["name"], url_path="/run", json=row)
-            await raise_for_status(res)
-            return await res.json()
-
-        return await tqdm.gather(*map(_post_subroutine, examples), desc="Collecting rollouts", miniters=10)
+        raise NotImplementedError
 
     def setup_server_client(self, head_server_config: Optional[BaseServerConfig] = None) -> ServerClient:
         server_client = ServerClient.load_from_global_config(head_server_config)
@@ -196,8 +183,8 @@ class RolloutCollectionHelper(BaseModel):  # pragma: no cover
         return server_client
 
 
-def collect_rollouts():  # pragma: no cover
-    config = RolloutCollectionConfig.model_validate(get_global_config_dict())
-    rch = RolloutCollectionHelper()
+def verify_offline():  # pragma: no cover
+    config = VerifyOfflineConfig.model_validate(get_global_config_dict())
+    helper = VerifyOfflineHelper()
 
-    asyncio.run(rch.run_from_config(config))
+    asyncio.run(helper.run_from_config(config))
