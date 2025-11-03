ed715ced3eef880e53e9fbb52850aa9f1002e2cd
abhibha-nvidia <abhibhag@nvidia.com>
2025-11-03 12:01:08 -0800

    remove api key

--- resources_servers/workbench/scripts/run_temp_penguin.sh
+++ resources_servers/workbench/scripts/run_temp_penguin.sh
@@ -1,4 +1,4 @@
-WANDB_API_KEY=ac17639e76dcbd0dbb3aea36aec1ae8286de56bc \
+WANDB_API_KEY=<wandb api key> \
 SLURM_ACCOUNT=llmservice_modelalignment_sft \
 SLURM_PARTITION=batch_block1 \
 EXP_NAME=penguin_grpo/qwen3_4binstruct/8nodes/grpo-qwen3-4b-instruct-pre-cot \
@@ -9,7 +9,7 @@ REPO_LOCATION=/lustre/fsw/portfolios/llmservice/users/abhibhag/nemo-rl \
     logger.wandb.project="$USER-nemo-gym-rl-integration"
 
 
-WANDB_API_KEY=ac17639e76dcbd0dbb3aea36aec1ae8286de56bc \
+WANDB_API_KEY=<wandb api key> \
 SLURM_ACCOUNT=llmservice_modelalignment_sft \
 SLURM_PARTITION=batch_block1 \
 EXP_NAME=penguin_grpo/qwen3_4binstruct/8nodes/grpo-qwen3-4b-instruct-pre-cot \
@@ -20,7 +20,7 @@ REPO_LOCATION=/lustre/fsw/portfolios/llmservice/users/abhibhag/nemo-rl \
     logger.wandb.project="$USER-nemo-gym-rl-integration"
 
 
-WANDB_API_KEY=ac17639e76dcbd0dbb3aea36aec1ae8286de56bc \
+WANDB_API_KEY=<wandb api key> \
 SLURM_ACCOUNT=llmservice_modelalignment_sft \
 SLURM_PARTITION=batch_block1 \
 EXP_NAME=penguin_grpo/qwen3_4binstruct/8nodes/grpo-qwen3-4b-instruct-pre-cot \
