7b9c66ad2e92ef6d6dfbbd9a6009c9705915ffa8
Peter Jin <pjin@nvidia.com>
2025-11-13 07:11:26 -0800

    Add test.
    Signed-off-by: Peter Jin <pjin@nvidia.com>

--- tests/unit_tests/test_openai_utils.py
+++ tests/unit_tests/test_openai_utils.py
@@ -12,9 +12,26 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from nemo_gym.openai_utils import NeMoGymAsyncOpenAI
+import uuid
+from unittest.mock import MagicMock
+
+from pytest import MonkeyPatch
+
+from nemo_gym.openai_utils import (
+    NeMoGymAsyncOpenAI,
+    empty_response,
+)
 
 
 class TestOpenAIUtils:
     async def test_NeMoGymAsyncOpenAI(self) -> None:
         NeMoGymAsyncOpenAI(api_key="abc", base_url="https://api.openai.com/v1")
+
+    def test_empty_response(self, monkeypatch: MonkeyPatch) -> None:
+        uuid_value = uuid.UUID("12345678123456781234567812345678")
+        uuid4_mock = MagicMock()
+        uuid4_mock.return_value = uuid_value
+        monkeypatch.setattr(uuid, "uuid4", uuid4_mock)
+        expected_response_id = f"resp_{uuid_value.hex}"
+        actual_response = empty_response()
+        assert actual_response.id == expected_response_id
